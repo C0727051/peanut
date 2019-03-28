2519f480d1254817e9812a034b49ba2bf3078c54
Manveer Singh Walia <727051@student.tor.loc>
2019-03-28 15:10:15 -0400

    comment/firstcommit

---
+++ manveer1/manveer1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manveer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Peter rules the IT Universe");
+        }
+    }
+}
